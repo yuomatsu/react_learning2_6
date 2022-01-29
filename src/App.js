2f656de098d01dedb6ec1adca5a056aa37114143
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-01-29 19:33:19 +0900

    31. Templateの作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molucules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "ゆう",
+  image: "https://source.unsplash.com/2l0CWTpcChI",
+  email: "12345@xxx.com",
+  phone: "000-0000-1000",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "fgerthyjhrgefwwgrhtjyhrge"
+};
 
 export default function App() {
   return (
@@ -9,6 +21,7 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SearchInput></SearchInput>
+      <UserCard user={user}></UserCard>
     </div>
   );
 }
